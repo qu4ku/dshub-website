75f1e8420c0e2fc66c06e8e16186cda1557e8d7d
qu4ku <qu4ku@hotmail.com>
2018-07-06 15:35:27 +0200

    gitignore fix

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kamilwroniewicz/_code/_github/180601-datahub-website/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
