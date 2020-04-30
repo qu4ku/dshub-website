432f333f08fc1eb92f42ac1669b350cdea17c42d
qu4ku <qu4ku@hotmail.com>
2020-04-30 11:02:50 +0200

    book & category models

--- src/books/models.py
+++ src/books/models.py
@@ -16,17 +16,45 @@ class BookCategory(models.Model):
 		ordering = ('title',)
 
 	# Do not turn it off once is on: seo
-	is_active = models.BooleanField(default=False)
-	title = models.CharField(max_length=200)
-	slug = models.SlugField(unique=True)
+	is_active = models.BooleanField(
+		help_text='If available the category page will be published online.',
+		default=False
+	)
+	title = models.CharField(
+		help_text='Title, used in meta tags by default',
+		max_length=200
+	)
+	slug = models.SlugField(
+		help_text='Slug for the category subpage.',
+		unique=True
+	)
 
 	# Used to generate subpage
-	headline = models.CharField(max_length=200, null=True, blank=True)
-	description = models.TextField(null=True, blank=True)
+	headline = models.CharField(
+		help_text='Headline/H1 text for the category subpage.',
+		max_length=200, 
+		null=True, 
+		blank=True
+	)
+	description = models.TextField(
+		help_text='Description/H2 text for the category subpage.',
+		null=True, 
+		blank=True
+	)
 
 	# Seo
-	seo_title = models.CharField(max_length=60, blank=True, null=True)
-	seo_description = models.CharField(max_length=165, blank=True, null=True)
+	seo_title = models.CharField(
+		help_text='Used in meta title tag if available.',
+		max_length=60, 
+		null=True,
+		blank=True
+	)
+	seo_description = models.CharField(
+		help_text='Used in meta description tag if available.',
+		max_length=165, 
+		null=True, 
+		blank=True
+	)
 
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
@@ -49,15 +77,23 @@ class Book(models.Model):
 
 	title = models.CharField(max_length=200)
 	author = models.CharField(max_length=200, null=True, blank=True)
-	rating_good_reads = models.CharField(max_length=10, null=True, blank=True, default='NA')
-	rating_amazon = models.CharField(max_length=10, null=True, blank=True, default='NA')
-	description = models.TextField(null=True, blank=True)
+	publication_date = models.DateField(null=True, blank=True)
+	rating_goodreads = models.CharField(max_length=20, null=True, blank=True, default='NA')
+	rating_amazon = models.CharField(max_length=20, null=True, blank=True, default='NA')
+	url_goodreads = models.URLField(
+		max_length=250,
+		help_text='Url to goodreads.',
+		null=True,
+		blank=True
+	)
 	url = models.URLField(
 		max_length=250,
 		help_text='Url to amazon.',
 	)
+	description = models.TextField(null=True, blank=True)
 	categories = models.ManyToManyField(BookCategory, blank=True)
 	book_image = models.ImageField(upload_to='book-covers/')
+	
 
 	is_active_category = models.BooleanField(
 		default=False,
@@ -82,6 +118,12 @@ class Book(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 
+
+	def get_publication_year(self):
+		if self.publication_date:
+			return self.publication_date.year
+		else:
+			return None
 
 	def __str__(self):
 		return self.title
